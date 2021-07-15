7407a7a9c119e79440866e0a5e0f90c09244e3d6
Alfons <39962260+Alfons44@users.noreply.github.com>
2021-07-15 22:09:40 +0200

    Update alf_billing.sql

--- Alf_Billing/alf_billing.sql
+++ Alf_Billing/alf_billing.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `alf_billing` (
   `sender` varchar(255) DEFAULT NULL,
-  `reciever` varchar(255) DEFAULT NULL,
+  `receiver` varchar(255) DEFAULT NULL,
   `target_type` varchar(255) DEFAULT NULL,
   `target` varchar(255) DEFAULT NULL,
   `label` varchar(255) DEFAULT NULL,
